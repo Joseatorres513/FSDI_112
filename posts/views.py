7b8f4ed4996aba91fc95dbe8983f59d9ab82588c
Jose Torres <jose.a.torres513@gmail.com>
2025-02-15 13:24:49 -0500

    FSDI_112_Assignment 3

--- posts/views.py
+++ posts/views.py
@@ -12,11 +12,11 @@ class PostListView(ListView):
     template_name = "posts/list.html"
     model = Post
 
-    class PostDetailView(DetailView):
+class PostDetailView(DetailView):
         template_name = "posts/detail.html"
         model = Post
 
-    class PostCreateView(CreateView):
+class PostCreateView(CreateView):
         template_name = "posts/new.html"
         model = Post
         fields = ["title", "subtitle", "body"]
